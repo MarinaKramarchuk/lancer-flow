bdf878fc8626d214374496bde4cd32ec613e12fc
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2026-01-28 17:52:42 +0200

    fixed the color of BarLaoder

--- app/(main)/dashboard/layout.tsx
+++ app/(main)/dashboard/layout.tsx
@@ -7,7 +7,7 @@ const DashboardLayout: React.FC = () => {
     <div className="px-5">
       <h1 className="text-6xl font-bold mb-5">Dashboard</h1>
 
-      <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}>
+      <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="#1c398e" />}>
         <DashboardPage />
       </Suspense>
     </div>
