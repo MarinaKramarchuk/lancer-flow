786896aa815e70f7cd8da783a600fc49e7e90c5e
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2026-03-26 11:53:43 +0200

    feat: implemented Arcjet shield and bot protection

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,32 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import arcjet from "@arcjet/next";
+import { detectBot, shield, createMiddleware } from "@arcjet/next";
 
 const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/account(.*)',
-  '/transaction(.*)',
+  "/dashboard(.*)",
+  "/account(.*)",
+  "/transaction(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+const ARCJET_KEY = process.env.ARCJET_KEY;
+
+if (!ARCJET_KEY) {
+  throw new Error("ARCJET_KEY is not defined");
+}
+const aj = arcjet({
+  key: ARCJET_KEY,
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
+
+const clerk = clerkMiddleware(async (auth, req) => {
   const authObject = await auth();
 
   if (!authObject.userId && isProtectedRoute(req)) {
@@ -14,9 +34,11 @@ export default clerkMiddleware(async (auth, req) => {
   }
 });
 
+export default createMiddleware(aj, clerk);
+
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
 };
